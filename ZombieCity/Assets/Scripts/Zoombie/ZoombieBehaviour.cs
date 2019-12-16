2468f8e8ac2db760446d2d1430d59e0b0ff5331c
gudinden <sandra@fagerli.in>
2019-12-16 13:51:49 +0100

    Assets + unity scene
    Assets + unity scene

---
+++ ZombieCity/Assets/Scripts/Zoombie/ZoombieBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoombieBehaviour : MonoBehaviour{
+    public GameObject Player;
+    public float movementSpeed = 4;
+    // public Transform player;
+    // private Rigidbody zoombiemove;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // zoombiemove = this.GetComponent<Ridigbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        //Vector3 direction = player.position - transform.position;
+        //Debug.Log(direction);
+
+    }
+}
