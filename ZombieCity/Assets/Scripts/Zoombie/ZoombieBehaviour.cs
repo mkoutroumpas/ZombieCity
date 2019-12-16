2d4022a60f42eae054189a44af757bf382eff674
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-12-16 14:47:27 +0100

    Added zombie movement snippet.

--- ZombieCity/Assets/Scripts/Zoombie/ZoombieBehaviour.cs
+++ ZombieCity/Assets/Scripts/Zoombie/ZoombieBehaviour.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class ZoombieBehaviour : MonoBehaviour
 {
     public GameObject ZombiePrefab;
-    
-    private Entity ZombieEntity;
 
+    private Entity ZombieEntity;
+    
+    public GameObject Player;
+    public float ZoombieMove = 4;
+    
     public void Start()
     {
         QualitySettings.vSyncCount = 0;
-
+        
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
         ZombieEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(ZombiePrefab, settings);
     }
@@ -21,10 +24,13 @@ public class ZoombieBehaviour : MonoBehaviour
     {
         var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
         var spawnZombie = entityManager.Instantiate(ZombieEntity);
-
+        
         entityManager.SetComponentData(spawnZombie, new Translation { Value = GetSpawnPointInWorldSpace() });
+        
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * ZoombieMove * Time.deltaTime;
     }
-
+    
     private float3 GetSpawnPointInWorldSpace()
     {
         return new float3(0f, 0f, 0f);
