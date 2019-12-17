335b7165aa77d825096413ca847e89488e0b77de
Marios Koutroumpas <mku@trackman.com>
2019-12-17 11:29:13 +0100

    Created ECS_Testbed.

---
+++ ZombieCity/Assets/ECS_Testbed/Scripts/ECS_Testbed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ECS_Testbed : MonoBehaviour
+{
+    [SerializeField]
+    private Transform ObjectToMove;
+    private List<ZMoveableObject> moveableObjects;
+
+    private void Update()
+    {
+        float startTime = Time.realtimeSinceStartup;
+
+
+
+        Debug.Log(((Time.realtimeSinceStartup - startTime) * 1000f) + "ms");
+    }
+
+
+
+    public class ZMoveableObject
+    {
+        public Transform Transform;
+        public float moveZ;
+    }
+}
+
---
+++ ZombieCity/Assets/ECS_Testbed/Scripts/ZTranslationJob.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+public class ZTranslationJob : IJobParallelFor
+{
+    [ReadOnly]
+    public NativeArray<float> MoveSpeeds;
+    public NativeArray<float3> Positions;
+
+    public float DeltaTime;
+
+    private bool _minusDirection;
+
+    public ZTranslationJob(bool minusDirection = false)
+    {
+        _minusDirection = minusDirection;
+    }
+
+    public void Execute(int index)
+    {
+        Positions[index] += (_minusDirection ? -1 : 1) * new float3(0f, 0f, MoveSpeeds[index] * DeltaTime);
+    }
+}
